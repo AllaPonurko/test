3298ff3d8e801451dc98c41e63eadd2e3543ab30
Alla Ponurko <ponurko@oksystem.cz>
2025-01-30 17:04:35 +0100

    refactoring test for create book

--- src/main/java/org/example/test/BookServiceTest.java
+++ src/main/java/org/example/test/BookServiceTest.java
@@ -1,8 +1,11 @@
 package org.example.test;
 
+import org.example.dto.BaseReq;
 import org.example.helper.TestHelper;
 import org.example.models.products.Book;
+import org.example.services.services.BookService;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Assertions;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -11,14 +14,21 @@ import org.testng.annotations.Test;
 @SpringBootTest
 public class BookServiceTest {
     @MockBean
-    private TestHelper testHelper;
+    private BookService bookService;
     @Test
     public void createBookTest()throws Exception{
         Book mockBook = new Book();
         mockBook.setName("Mock title");
-        Mockito.when(testHelper.createBook()).thenReturn(mockBook);
-      Book newBook=testHelper.createBook();
-      Assertions.assertEquals("Mock Title", newBook.getName());
+        var newBookreq= new BaseReq("NewBook",
+                21.00,
+                "this is a new book of well-known author",
+                "book",
+                "Old author",
+                "New genre",
+                "");
+        Mockito.when(bookService.createProduct(Mockito.any(BaseReq.class))).thenReturn(mockBook);
+      Book newBook=bookService.createProduct(newBookreq);
+      Assertions.assertEquals("Mock Title", mockBook.getName());
       Assertions.assertTrue(newBook!=null,"Book is created successful");
     }
 }
